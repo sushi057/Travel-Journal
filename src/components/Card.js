e31f7e2b8411c6f2dd8a0d61f221889bf94b114a
suvash <suvash2077@gmail.com>
2022-11-26 23:36:57 +0545

    Card.css finished

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,21 @@
-import React from "react";
+import "../styles/card.css";
 
 export default function Card(props) {
   return (
     <div className="main-card">
-      <img src={props.item.imageUrl} alt="location" />
+      <img className="main-location" src={props.item.imageUrl} alt="location" />
       <div className="main-details">
-        <p>
-          <img src="/images/landmark.png" alt="landmark" />
-          {props.item.location}{" "}
+        <p className="main-top">
+          <img className="landmark" src="/images/landmark.png" alt="landmark" />
+          <span className="main-place">{props.item.location}</span>
           <a href={props.item.googleMapsUrl}>View on Google Maps</a>
         </p>
 
         <h2>{props.item.title}</h2>
-        <p>
+        <p className="main-date">
           {props.item.startDate} - {props.item.endDate}
         </p>
-        <p>{props.item.description}</p>
+        <p className="main-description">{props.item.description}</p>
       </div>
     </div>
   );
